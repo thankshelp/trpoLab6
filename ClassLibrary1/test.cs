36ca7b3f575d9e7ad59aee6e401133e7f6a1605d
thankshelp <taraninav.44@gmail.com>
2019-04-02 12:56:48 +0700

    add see

--- ClassLibrary1/test.cs
+++ ClassLibrary1/test.cs
@@ -29,14 +29,22 @@ namespace ClassLibrary1
         }
 
         [TestCase]
-        public void google_test()
+        public void search_test()
         {
-            webDriver.Url = "https://www.google.ru/";
-            IWebElement search = webDriver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input"));
-            search.SendKeys("Функциональное тестирование");
+            webDriver.Url = "https://sibsutis.ru/";
+            IWebElement search = webDriver.FindElement(By.XPath("//*[@id=\"qqq\"]"));
+            search.SendKeys("мрм");
 
-            IWebElement button = webDriver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[3]/center/input[1]"));
+            IWebElement button = webDriver.FindElement(By.XPath("//*[@id=\"layout\"]/div[2]/div[2]/div[2]/div[3]/form/input[2]"));
             button.Click();
+        }
+
+        [TestCase]
+        public void see_test()
+        {
+            webDriver.Url = "https://sibsutis.ru/";
+            IWebElement element = webDriver.FindElement(By.XPath("//*[@id=\"layout\"]/div[2]/div[2]/div[1]/ul/li[2]/a"));
+            bool status = element.Enabled;
         }
     }
 }
