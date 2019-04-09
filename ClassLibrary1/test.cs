4bed0b1edbf597f9293f3962ea004d4c16958a0f
thankshelp <taraninav.44@gmail.com>
2019-04-09 13:09:22 +0700

    the end

--- ClassLibrary1/test.cs
+++ ClassLibrary1/test.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace ClassLibrary1
 {
@@ -19,15 +20,9 @@ namespace ClassLibrary1
         {
             webDriver.Url = "https://shikimori.org/";
             Assert.AreEqual("Шикимори - энциклопедия аниме и манги", webDriver.Title);
-            webDriver.Close();
+            
         }
-
-        [TearDown]
-        public void testEnd()
-        {
-            webDriver.Quit();
-        }
-
+        
         [TestCase]
         public void search_test()
         {
@@ -36,8 +31,11 @@ namespace ClassLibrary1
             button.Click();
 
             IWebElement search = webDriver.FindElement(By.XPath("//*[@id=\"dashboards_show\"]/header/div[2]/div[3]/div/div/input"));
-            search.SendKeys("хёка");
-            
+            search.SendKeys("наруто");
+            search.SendKeys(Keys.Return);
+
+            Assert.AreEqual("https://shikimori.org/animes?search=%D0%BD%D0%B0%D1%80%D1%83%D1%82%D0%BE", webDriver.Url);
+
         }
 
         [TestCase]
@@ -46,17 +44,56 @@ namespace ClassLibrary1
             webDriver.Url = "https://shikimori.org/";
             IWebElement element = webDriver.FindElement(By.XPath("//*[@id=\"dashboards_show\"]/section/div[2]/div[1]/div[1]/a"));
             bool status = element.Enabled;
+            Assert.AreEqual(true, status);
         }
 
         [TestCase]
         public void link_test()
         {
             webDriver.Url = "https://shikimori.org/";
-            IWebElement button = webDriver.FindElement(By.XPath("//*[@id=\"dashboards_show\"]/header/div[2]/div[1]/div[2]"));
-            button.Click();
-
-            IWebElement element = webDriver.FindElement(By.XPath("//*[@id=\"dashboards_show\"]/header/div[2]/div[3]/div/ul/li[1]"));
+            
+            IWebElement element = webDriver.FindElement(By.XPath("//*[@id=\"dashboards_show\"]/section/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/a[2]"));
             element.Click();
+
+            //element = webDriver.FindElement(By.XPath("//*[@id=\"animes_collection_index\"]/section/div/header/h1"));
+            //bool status = element.Displayed;
+
+            //Assert.AreEqual(true, status);
+
+
+            var wait = new WebDriverWait(webDriver, new TimeSpan(0, 0, 30));
+            //установка условия окончания ожидания
+            var ele = wait.Until(condition =>
+            {
+                try
+                {
+                    //до тех пор, пока указанный элемент не станет видим (Displayed == true)
+                    if (webDriver.Title == "Аниме лета 2019")
+                        return true;
+                    else return false;
+                }
+                //в случае, если такого элемента нет
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+
+            Assert.AreEqual("Аниме лета 2019", webDriver.Title);
+
+          //  Assert.AreEqual("https://shikimori.org/animes/season/summer_2019", webDriver.Url);
+
         }
+       
+        //[TearDown]
+        //public void testEnd()
+        //{
+        //    webDriver.Quit();
+        //}
+       
     }
 }
